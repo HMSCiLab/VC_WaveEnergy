6a21fda14c10600c28d90cb753d9840f91df500f
shindelr <129821483+shindelr@users.noreply.github.com>
2026-03-23 11:58:04 -0400

    Added loading spinner for data button

---
+++ vc-wave-energy/src/components/LoadingSpinner.tsx
@@ -0,0 +1,18 @@
+const LoadingSpinner = () => {
+  return (
+    <div
+      className="
+        absolute
+        animate-spin 
+        rounded-full 
+        h-24 
+        w-24 
+        border-t-4 
+        border-b-4 
+        border-blue-500
+      "
+    ></div>
+  );
+};
+
+export default LoadingSpinner;
